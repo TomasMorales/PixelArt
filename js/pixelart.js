34bbf99bf63638f694f56211589c56fd5e472e70
TomasMorales <Marcelo.Morales@alu.bue.edu.ar>
2016-10-24 11:37:20 -0300

    version 1

---
+++ js/pixelart.js
@@ -0,0 +1,47 @@
+var myPixelDraw={
+	colorPicked:0,
+
+	defaultCells: 30,
+	coloring: false,
+	fns:{
+		calcSize:function (cant) {
+			console.log('calcSize');
+		},
+		reSize:function (cant) {
+			console.log('reSize');
+		},
+		detectMouseUp:function (cant) {
+			console.log('detectMouseUp');
+		},
+		colorPalette:function (cant) {
+			console.log('colorPalette');
+		},
+		pickColor:function (cant) {
+			console.log('pickColor');
+		},
+		colorIt:function (cant) {
+			console.log('colorIt');
+		},
+		colorOnDrag:function (cant) {
+			console.log('colorOnDrag');
+		},
+		reset:function (cant) {
+			console.log('reset');
+		},
+		toggleBorders:function (cant) {
+			console.log('toggleBorders');
+		},
+		disableRightClick:function (cant) {
+			console.log('disableRightClick');
+		},
+		grabImage:function (cant) {
+			console.log('grabImage');
+		}
+
+	}
+
+}
+
+$(function(){
+	
+})
